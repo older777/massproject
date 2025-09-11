e40bb2320693daa112412819e86adf5a0b2c1ebf
older777 <kkkart@mail.ru>
2025-09-11 09:36:51 +0300

    Gitignore. Seeder update

--- database/seeders/AddAdminSeeder.php
+++ database/seeders/AddAdminSeeder.php
@@ -1,22 +1,21 @@
 <?php
+
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
 use App\Models\User;
 use Carbon\Carbon;
+use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
 class AddAdminSeeder extends Seeder
 {
-
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
         User::firstOrCreate([
-            'name' => 'admin'
+            'name' => 'admin',
         ], [
             'name' => 'admin',
             'email' => 'test@example.com',
